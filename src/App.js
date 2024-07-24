75f96253b2da3c8e83838bbe0f5e2e029aae681c
Gia Cuong <legiacuong789@gmai.com>
2024-07-24 14:51:20 +0700

    [Homepage] Fixed error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Everything from "./components/Everything";
-import Tech from "./components/Techs";
+import Tech from "./components/Tech";
 import Build from "./components/Build";
 import Footer from "./components/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -10,7 +10,7 @@ import Contact from "./components/Contact";
 import Pricing from "./components/Pricing";
 import About from "./components/About";
 import Blog from "./components/Blog";
-import Error from "./components/404";
+import Error from "./components/Error";
 
 function App() {
   return (
