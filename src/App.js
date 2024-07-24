8ed1991f9bcd4e0bc9110c52c7949af47d568607
Gia Cuong <legiacuong789@gmai.com>
2024-07-24 14:58:18 +0700

    [App.js] Move the router on the top

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Everything from "./components/Everything";
 import Tech from "./components/Tech";
 import Build from "./components/Build";
 import Footer from "./components/Footer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Contact from "./components/Contact";
 import Pricing from "./components/Pricing";
 import About from "./components/About";
