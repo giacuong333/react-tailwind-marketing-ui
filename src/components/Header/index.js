28c77beb58f06ebc9088b46213e57d083fdb28ed
Gia Cuong <legiacuong789@gmai.com>
2024-07-24 16:05:55 +0700

    Last deploy

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,7 +15,7 @@ function Header() {
     <header className="max-w-[1280px] w-full px-4 mx-auto my-5">
       {/* Medium and large screen */}
       <div className="md:flex hidden items-center justify-between">
-        <Link to={"/"} className="text-lg cursor-pointer">
+        <Link to={"/react-tailwind-marketing-ui"} className="text-lg cursor-pointer">
           <span className="font-bold text-slate-800">Astro</span>
           <span className="text-slate-500">ship</span>
         </Link>
@@ -85,10 +85,10 @@ function Header() {
 
       {/* Small screen */}
       <div className="md:hidden block">
-        <h3 className="text-lg cursor-pointer inline-block">
+        <Link to={"/react-tailwind-marketing-ui"} className="text-lg cursor-pointer inline-block">
           <span className="font-bold text-slate-800">Astro</span>
           <span className="text-slate-500">ship</span>
-        </h3>
+        </Link>
 
         <div onClick={handleNav} className="flex cursor-pointer float-right">
           {nav ? <IoClose size={24} /> : <FaBars size={20} />}
@@ -98,10 +98,31 @@ function Header() {
           <div>
             <ul className="block items-center gap-3 mt-2">
               <li className="text-gray-600 hover:text-gray-900 cursor-pointer duration-300">
-                <div className="inline-flex py-2 md:px-3 items-center">
-                  <span className="mr-1">Features</span>
-                  <IoIosArrowDown size={14} className="mt-0.5" />
-                </div>
+                <Tippy
+                  interactive
+                  trigger="click"
+                  placement="right-end"
+                  content={
+                    <ul className="bg-white rounded-md shadow border py-2 px-3">
+                      <li className="py-1 text-gray-600  hover:text-gray-900">
+                        <Link to={""}>Action</Link>
+                      </li>
+                      <li className="py-1 text-gray-600  hover:text-gray-900">
+                        <Link to={""}>Another action</Link>
+                      </li>
+                      <li className="py-1 text-gray-600  hover:text-gray-900">
+                        <Link to={""}>Dropdown Submenu</Link>
+                      </li>
+                      <li className="py-1 text-gray-600  hover:text-gray-900">
+                        <Link to={"/404"}>404 Page</Link>
+                      </li>
+                    </ul>
+                  }>
+                  <div className="inline-flex py-2 md:px-3 items-center">
+                    <span className="mr-1">Features</span>
+                    <IoIosArrowDown size={14} className="mt-0.5" />
+                  </div>
+                </Tippy>
               </li>
               <li className="text-gray-600 hover:text-gray-900 cursor-pointer duration-300">
                 <Link to={"/pricing"} className="inline-block py-2 md:px-3">
